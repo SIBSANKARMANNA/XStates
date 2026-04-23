9cd7e624b9ac84303c81ccadb018fecd714ab5ab
sibsankarmanna <manna.sibsankar8@gmail.com>
2026-04-23 10:55:27 +0530

    update stateUpdate and disabled key

--- src/Cities.jsx
+++ src/Cities.jsx
@@ -10,8 +10,8 @@ function Cities() {
 
 
 
-    const {data:states, loading: statesLoading, error: statesError} = useFetch(country?`${base_url}/country=${country}/states`:null);
-    const {data:cities, loading: citiesLoading, error: citiesError} = useFetch(state?`${base_url}/country=${country}/state=${state}/cities`:null);
+    const {data:states=[], loading: statesLoading, error: statesError} = useFetch(country?`${base_url}/country=${country}/states`:null);
+    const {data:cities=[], loading: citiesLoading, error: citiesError} = useFetch(state?`${base_url}/country=${country}/state=${state}/cities`:null);
 
 
     if (loading) return <div>Countries Loading...</div>;
@@ -23,37 +23,36 @@ function Cities() {
             <h1 style={{textAlign: 'center'}}>Select Locations</h1>
 
             <div style={{textAlign: 'center', marginBottom: '20px'}}>
-                <select value={country} onChange={(e) => setCountry(e.target.value)}>
+                <select value={country} onChange={(e) => {setCountry(e.target.value);
+                    setState('');
+                    setCity('');
+                }}>
                     <option value="">Select Country</option>
                     {countries.map((c) => (
                         <option key={c} value={c}>{c}</option>
                     ))}
                 </select>
-                {country && (
-                    <>
-                        {statesLoading && <p>Loading States...</p>}
-                        {statesError && <div>Error in States: {statesError}</div>}
-                        <select value={state} onChange={(e) => setState(e.target.value)}>
-                            <option value="">Select State</option>
-                            {states.map((s) => (
-                                <option key={s} value={s}>{s}</option>
-                            ))}
-                        </select>
-                    </>
-                )}
 
-                {state && (
-                    <>
-                        {citiesLoading && <p>Loading Cities...</p>}
-                        {citiesError && <div>Error in Cities: {citiesError}</div>}
-                        <select value={city} onChange={(e) => setCity(e.target.value)}>
-                            <option value="">Select City</option>
-                            {cities.map((c) => (
-                                <option key={c} value={c}>{c}</option>
-                            ))}
-                        </select>
-                    </>
-                )}
+                <select value={state} disabled={!country} onChange={(e) => {setState(e.target.value);
+                    setCity('');
+                }}>
+                    <option value="">Select State</option>
+                    {statesLoading && <option>Loading...</option>}
+                    {statesError && <option>Error: {statesError}</option>}
+                    {states.map((s) => (
+                        <option key={s} value={s}>{s}</option>
+                    ))}
+                </select>
+
+
+                <select value={city} disabled={!state} onChange={(e) => setCity(e.target.value)}>
+                    <option value="">Select City</option>
+                    {citiesLoading && <option>Loading...</option>}
+                    {citiesError && <option>Error: {citiesError}</option>}
+                    {cities.map((c) => (
+                        <option key={c} value={c}>{c}</option>
+                    ))}
+                </select>
             </div>
 
             {country && state && city && (
